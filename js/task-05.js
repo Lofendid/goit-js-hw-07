249d1fec134dbec3be61904e502cfa30e618f622
Lofendid <102857014+Lofendid@users.noreply.github.com>
2023-12-05 22:09:10 +0100

    is-ready

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const handleClick = () => { 
+  const currentColor = body.style.backgroundColor = getRandomHexColor();
+  colorInfo.textContent = currentColor;
+};
+
+const colorInfo = document.querySelector('.color');
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+changeColorBtn.addEventListener('click', handleClick);
+
+
